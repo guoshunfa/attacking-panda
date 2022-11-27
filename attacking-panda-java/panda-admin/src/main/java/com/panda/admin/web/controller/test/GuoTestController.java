d1687b65078fe5f2574378c764175db0c76ad2e7
郭顺发 <13301203454@163.com>
2022-11-27 17:10:23 +0800

    添加测试类

---
+++ attacking-panda-java/panda-admin/src/main/java/com/panda/admin/web/controller/test/GuoTestController.java
@@ -0,0 +1,9 @@
+package com.panda.admin.web.controller.test;
+
+/**
+ * @className: GuoTestController
+ * @author: GuoShunFa
+ * @date: 2022/11/27
+ **/
+public class GuoTestController {
+}
---
+++ attacking-panda-java/panda-admin/src/main/java/com/panda/admin/web/controller/test/MaTestController.java
@@ -0,0 +1,9 @@
+package com.panda.admin.web.controller.test;
+
+/**
+ * @className: MaTestController
+ * @author: GuoShunFa
+ * @date: 2022/11/27
+ **/
+public class MaTestController {
+}
